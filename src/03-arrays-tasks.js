9db92cdc90579af97d7667e51787225991f421b1
Gar <gar@tut.by>
2022-07-26 20:45:31 +0300

    feat: solve 90 tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -605,8 +605,13 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  let answer = arr;
+  indexes.map((item) => {
+    answer = answer[item];
+    return item;
+  });
+  return answer;
 }
 
 /**
